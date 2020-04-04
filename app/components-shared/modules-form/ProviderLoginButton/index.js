bc1fecf6407995030f7b3f39da2dd780c7b99093
CarlPhilippe <carl@ceacle.com>
2020-04-04 15:09:24 -0700

    Update index.js

--- app/components-shared/modules-form/ProviderLoginButton/index.js
+++ app/components-shared/modules-form/ProviderLoginButton/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { authProvider } from 'services/config'
+import { authProvider } from 'appConfig'
 import FacebookLogin from './FacebookLogin'
 import GoogleLogin from './GoogleLogin'
 
