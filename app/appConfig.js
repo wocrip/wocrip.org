a30fa1fba22c78beb65076f5dfe3830ba29a32d4
CarlPhilippe <carl@ceacle.com>
2020-04-04 15:08:47 -0700

    Update appConfig.js

--- app/appConfig.js
+++ app/appConfig.js
@@ -1,3 +1,25 @@
+import { isDev } from 'utils/helpers'
+
+// @TODO: Merge the two export
+export const stripeApiKey = isDev ?
+  '' :
+  ''
+
+export const authEndpoint = isDev ?
+  'http://localhost:3071/graphql' :
+  'https://wocrip.herokuapp.com/graphql'
+
+export const authProvider = isDev ?
+  {
+    facebookID: '',
+    googleID: '',
+  }
+  :
+  {
+    facebookID: '',
+    googleID: '',
+  }
+
 export default {
   googleAnalyticsID: 'UA-162504335-1',
 }
