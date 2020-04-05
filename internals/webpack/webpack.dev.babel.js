b2fccf073260ff30410e1e2a67a986820f000872
CarlPhilippe <carl@ceacle.com>
2020-04-04 17:21:16 -0700

    Reverts to cheap-module-eval-source-map

--- internals/webpack/webpack.dev.babel.js
+++ internals/webpack/webpack.dev.babel.js
@@ -50,8 +50,7 @@ module.exports = require('./webpack.base.babel')({
 
   // Emit a source map for easier debugging
   // See https://webpack.js.org/configuration/devtool/#devtool
-  // devtool: 'cheap-module-eval-source-map',
-  devtool: 'eval',
+  devtool: 'cheap-module-eval-source-map',
 
   performance: {
     hints: false,
